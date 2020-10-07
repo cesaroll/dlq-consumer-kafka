c6a127c055f810bd88d935cf9ea926033eafa265
Cesar Lopez <clopezlerma@wayfair.com>
2020-10-07 19:25:11 +0200

    Saving into Mongo cluster

--- consumer.py
+++ consumer.py
@@ -1,5 +1,6 @@
 from kafka import KafkaConsumer
 from pymongo import MongoClient
+import json
 
 consumer = KafkaConsumer(
     'dlq-topic',
@@ -8,8 +9,13 @@ consumer = KafkaConsumer(
      enable_auto_commit=True,
      group_id='dlq-consumer-group')
 
+client = MongoClient('mongodb+srv://test:aAaNuMGzuck1y2YX@cluster0.o6w2h.gcp.mongodb.net/dlq?retryWrites=true&w=majority')
+db = client.dlq
+collection = db.poisoned_messages
+
 for message in consumer:
   message = message.value.decode('utf-8')
   print('Message: {}'.format(message))
-
-
+  dict = json.loads(message)
+  messageId = collection.insert_one(dict).inserted_id
+  print('Inserted Id: {}'.format(messageId))
